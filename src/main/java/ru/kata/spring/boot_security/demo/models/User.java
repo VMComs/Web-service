74b075eb4e77de130daae2b65207d19bd0752d43
VMComs <110674656+VMComs@users.noreply.github.com>
2023-01-10 14:15:59 +0300

    Update User.java

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -37,7 +37,7 @@ public class User implements UserDetails {
 //    @NonNull
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name="users_roles",
     joinColumns = @JoinColumn(name="user_id"),
     inverseJoinColumns = @JoinColumn(name="role_id"))
